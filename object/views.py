e15e482915be2222f355298acd5bde297b86808f
Jakob Hoeper <jhoeper@bbaw.de>
2018-07-16 19:02:35 +0200

    add html templates and search view for object

--- object/views.py
+++ object/views.py
@@ -5,6 +5,15 @@ from django.views.decorators.http import require_http_methods
 
 import store
 
+def param_pop(params, key, default):
+    values = params.pop(key, [default])
+    if len(values) < 2:
+        return values[0]
+    else:
+        return values
+
+
+
 @require_http_methods(["GET"])
 def details(request, obj_id):
     obj = store.get('object', obj_id)
@@ -17,7 +26,18 @@ def details(request, obj_id):
 @require_http_methods(["GET"])
 def search(request):
     params = request.GET.copy()
-    return JsonResponse(params, safe=False)
+    size = int(param_pop(params, 'size', 100))
+    page = int(param_pop(params, 'page', 1))
+
+    query = store.lucenify(params)
+    hits = store.search('object', query, size=size, offset=(page-1)*size)
+    
+    return render(request, 'object/search.html', {
+        'name': params.get('name', ''),
+        'hits': store.hits_contents(hits),
+        'pagination': store.pagination(page, size, hits.get('total')),
+        'parameters': '&'.join(['{}={}'.format(k, v) for k, v in params.items()])
+        })
 
 
 
