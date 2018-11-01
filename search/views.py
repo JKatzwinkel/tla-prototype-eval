6c10f8aa4ffccd0ae08e6fdaa7e99fe3b291368c
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2018-11-01 22:54:27 +0100

    use correct template path in view

--- search/views.py
+++ search/views.py
@@ -75,6 +75,6 @@ def search(request):
 @require_http_methods(["GET"])
 def search_dict(request):
     params = request.GET.copy()
-    return render(request, 'search/lemma.html', {})
+    return render(request, 'search/dict.html', {})
 
 # Create your views here.
