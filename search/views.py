273ed74da50914e18f06b8d696ae721d899f6194
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2018-11-04 22:42:51 +0100

    prepare for nested search result lists

--- search/views.py
+++ search/views.py
@@ -65,19 +65,36 @@ WORD_CLASSES = {
 
 
 def dict_search_query(**params):
+    """ generate elasticsearch query object with parameters as would be expected to come from
+    the `dict-search` form in the `search/index.html` template. """
     q = {"query": {"bool": {"must": []}}}
     if 'transcription' in params:
         q['query']['bool']['must'].append(
-                {
-                    "match": {
-                        "name": params.get('transcription')[0]
-                        }
-                    })
+            {
+                "match": {
+                    "name": params.get('transcription')[0]
+                    }
+                })
     return q
 
 
 def hit_tree(hits):
-    return [h for h in hits]
+    """ extracts the implicit hierarchical structure among the given objects """
+    structure = {h.get('id'):h for h in hits}
+    res = []
+    while len(hits) > 0:
+        h = hits.pop(0)
+        res.append((range(0), None, h))
+        for pred in ['rootOf', 'successor']:
+            for rel in h.get('relations', {}).get(pred, []):
+                if structure.get(rel.get('id')) in hits:
+                    obj = hits.pop(
+                            hits.index(
+                                structure.get(rel.get('id'))
+                                )
+                            )
+                    res.append((range(1), pred, obj))
+    return res
 
 
 @require_http_methods(["GET"])
@@ -93,7 +110,6 @@ def search(request):
 @require_http_methods(["GET"])
 def search_dict(request):
     params = request.GET.copy()
-    print(params)
     hits = store.search('wlist',
             dict_search_query(**params),
             offset=params.get('start', 1)-1,
@@ -102,12 +118,12 @@ def search_dict(request):
     count = hits.get('total')
     hits = store.hits_contents(hits)
     hits = hit_tree(hits)
-    print(hits)
     return render(request, 'search/dict.html',
             {
                 'hits': hits,
                 'hitcount': count,
-                'start': params.get('start', 1)
+                'start': params.get('start', 1),
+                'end': min(count, params.get('start', 1)+14),
                 })
 
 
