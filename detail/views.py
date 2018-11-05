b192c4e263de91eafb079fea16719780fdfd1ae2
Jakob Hoeper <jhoeper@bbaw.de>
2018-11-05 12:35:52 +0100

    install glom for transformation in view

--- detail/views.py
+++ detail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from glom import glom, Coalesce
 
 import store
 
@@ -8,8 +9,17 @@ import store
 require_http_methods(["GET"])
 def lemma_details_page(request, lemma_id):
     lemma = store.get('wlist', lemma_id)
+    bibl = glom(lemma,
+            Coalesce(
+                (
+                    ('passport.bibliography.bibliographical_text_field',
+                        lambda x:x.split(';')),
+                    [str.strip]),
+                default=[])
+            )
     return render(request, 'details/lemma.html', {
         'lemma': lemma,
+        'bibl': bibl,
         })
 
 
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.300"
+
+setup(
+        name='aaew-tla-lite',
+        version=VERSION,
+        packages=find_packages(),
+        scripts=[
+            'manage.py',
+            ],
+        install_requires=[
+            "elasticsearch",
+            "glom",
+            "django",
+            ],
+        )
