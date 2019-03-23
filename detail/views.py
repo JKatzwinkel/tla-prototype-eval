194cdfdd6d55bf47026f94be943b675fdeb81372
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2019-03-23 01:19:09 +0100

    adjust to new passport schema and render lemma annotations

--- detail/views.py
+++ detail/views.py
@@ -1,9 +1,20 @@
+import re
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 from glom import glom, Coalesce
 
 import store
+
+
+def tag_transcription(string):
+    return re.sub(
+        r'\$([^$]*)\$',
+        r'<span class="bbaw-libertine">\1</span>',
+        string
+    )
+
 
 def occurence_count(lemma_id):
     count = store.es.count(
@@ -20,17 +31,58 @@ def occurence_count(lemma_id):
     return count
 
 
-require_http_methods(["GET"])
+def lemma_annotations(lemma_id):
+    """ find a lemma's annotations in es """
+    hits = store.search(
+        'annotation',
+        'relations.partOf.id:{}'.format(lemma_id),
+        size=100,
+    )
+    return store.hits_contents(hits)
+
+
+def render_annotations(annos):
+    for anno in annos:
+        textcontent = glom(
+            anno,
+            Coalesce(
+                'passport.annotation.0.lemma.0',
+                default=''
+            )
+        )
+        textcontent = re.sub(
+            r'\n',
+            '<br/>',
+            textcontent
+        )
+        anno['body'] = tag_transcription(textcontent)
+        anno['title'] = tag_transcription(anno['title'])
+    return annos
+
+
+@require_http_methods(["GET"])
 def lemma_details_page(request, lemma_id):
     lemma = store.get('wlist', lemma_id)
-    bibl = glom(lemma,
-            Coalesce(
+    bibl = glom(
+        lemma,
+        Coalesce(
+            (
                 (
-                    ('passport.bibliography.bibliographical_text_field',
-                        lambda x:x.split(';')),
-                    [str.strip]),
-                default=[])
-            )
+                    'passport.bibliography.0.bibliographical_text_field.0',
+                    lambda x: x.split(';')
+                ),
+                [str.strip]
+            ),
+            default=[]
+        )
+    )
+    lemma['relations'] = {
+        predicate: [
+            relation
+            for relation in relations
+            if relation.get('eclass') == 'BTSLemmaEntry'
+        ] for predicate, relations in lemma['relations'].items()
+    }
     return render(
         request,
         'details/l.html',
@@ -39,8 +91,11 @@ def lemma_details_page(request, lemma_id):
             'bibl': bibl,
             'occurences': {
                 'corpus': occurence_count(lemma_id),
-            }
+            },
+            'annotations': render_annotations(
+                lemma_annotations(
+                    lemma_id
+                )
+            ),
         }
     )
-
-
