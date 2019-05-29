71d0665a0c3f0b11305d32aee9485b854aa8291f
Jakob Hoeper <jhoeper@bbaw.de>
2019-05-29 19:49:42 +0200

    fix misspelling

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ from search import views
 urlpatterns = [
     path('', views.search),
     path('dict', views.search_dict),
-    path('occurences', views.search_text_words),
+    path('occurrences', views.search_text_words),
 ]
