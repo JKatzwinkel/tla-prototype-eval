54eef4d5ce24b8f611a7bc8402623f469cc8f18f
Daniel Werning <33833393+dwerning@users.noreply.github.com>
2019-10-07 01:54:10 +0200

    Dummies für statische Info(u.ä.)-Seiten erstellt
    ToDo: a) Inhalte dieser Seiten; b) seitenspezifische Help-Seiten anlegen

---
+++ webd/help/views.py
@@ -0,0 +1,57 @@
+import re
+from datetime import datetime
+
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.utils.http import urlencode
+
+from glom import glom, Coalesce, flatten
+
+import store
+    
+tlaTitle = "Thesaurus Linguae Aegyptiae"
+tlaVersion = "19"
+tlaIssue = "1"
+tlaReleaseDate = "30.10.2019"
+tlaEditor = "Berlin-Brandenburgische Akademie der Wissenschaften & Sächsische Akademie der Wissenschaften"
+tlaPublisher = "Berlin-Brandenburgische Akademie der Wissenschaften"
+tlaBaseURL = "http://tla.bbaw.de"
+
+def coins_openurl_kev(doc):
+    # generate a contextobject referent
+    # https://groups.niso.org/apps/group_public/download.php/14833/z39_88_2004_r2010.pdf
+    ctx_rft = [
+        ('ctx_ver', "Z39.88-2004"),
+        ('ctx_enc', "info.ofi/enc:UTF-8"),
+        ('ctx_tim', datetime.now().isoformat()),
+        ('rft.language', "en-US"),
+        ('rft.au', glom(doc, "edited.name")),
+        ('rft.genre', "article"),
+        ('rft.atitle', 'Lemma {} - {}'.format(
+            doc.get("id"),
+            doc.get("name"),
+        )),
+        ('rft.jtitle', tlaTitle),
+        ('rft.stitle', "TLA"),
+        ('rft.volume', tlaVersion),
+        ('rft.issue', tlaIssue),
+        ('rft.date', glom(doc, "edited.date")),
+        ('rft.place', 'Berlin'),
+        ('rft.publisher', tlaPublisher),
+        ('rft_val_fmt', 'info:ofi/fmt:kev:mtx:journal'),
+        ('url_ver', "Z39.88-2004"),
+    ]
+    coins_kev = urlencode(ctx_rft)
+    return coins_kev
+
+
+@require_http_methods(["GET"])
+def help(request):
+    params = request.GET.copy()
+    return render(
+        request,
+        'help/help.html',
+        {
+        }
+    )
+ 
