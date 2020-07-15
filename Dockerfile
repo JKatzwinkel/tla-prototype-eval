0c6f8b39731aa0c2940a0fec7209a1844d7be8db
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-07-15 11:59:16 +0200

    run corpus data ingestion server instance in seperate container

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,24 @@
-FROM python:alpine AS stage
+FROM python:alpine AS clone
 
 WORKDIR /app
 COPY . /app
 
 RUN apk upgrade --no-cache \
-	&& apk add --no-cache bash wget curl tar git \
+    && apk add --no-cache bash git \
     && git submodule update --init --recursive \
-    && rm -rf .git/ \
+    && rm -rf .git/
 
 
-FROM python:alpine
+FROM clone AS run
 
 ENV PYTHONUNBUFFERED=1
 ENTRYPOINT ["/app/resources/scripts/entrypoint.sh"]
 
 WORKDIR /app
-COPY --from=stage /app /app
+COPY --from=clone /app/ /app
 
-RUN pip install -e ./webd \
+RUN apk upgrade --no-cache \
+    && apk add --no-cache bash wget curl tar git \
+    && pip install -e ./webd \
     && chmod +x ./resources/scripts/entrypoint.sh
 
--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 CMD=runserver
+DAT=sample.tar.gz
+DIR=resources
 
 if [ $# -ge 1 ]; then
-    if [[ "$1" -eq "ingest" ]]; then
+    if [[ "$1" = "ingest" ]]; then
         CMD=populate
         echo "download && extract corpus file at ${SAMPLE_URL}.."
-        wget -q "${SAMPLE_URL}" -O sample.tar.gz
-        tar -xzf sample.tar.gz -C resources
+        wget -q "${SAMPLE_URL}" -O "${DAT}"
+        tar -xzf sample.tar.gz -C "${DIR}"
     fi
 fi
 
@@ -18,13 +20,18 @@ while true; do
 	sleep 4
 done
 
-if [[ "${CMD}" -eq "populate" ]]; then
+if [[ "${CMD}" = "populate" ]]; then
     if [ -e "resources/corpus/" ]; then
+        echo "ingest corpus data..."
         python webd/manage.py populate_indices
+        echo "rm corpus data..."
+        rm "${DAT}"
+        rm -r "${DIR}/corpus/"
     fi
 fi
 
-if [[ "${CMD}" -eq "runserver" ]]; then
+if [[ "${CMD}" = "runserver" ]]; then
+    echo "start server..."
     python webd/manage.py migrate
     python webd/manage.py runserver 0.0.0.0:8000
 fi
